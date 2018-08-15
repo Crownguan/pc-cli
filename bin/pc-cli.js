75afbb7b2c3319bee3e84506d68af84c576edb16
王冠 <wangguan@tuhu.cn>
2018-08-15 17:51:26 +0800

    cli

--- bin/pc-cli.js
+++ bin/pc-cli.js
@@ -1,12 +1,14 @@
+ //usr/bin/env node
+
 const program = require('commander')
 
 program
-  .version(require('../package.json').version)
-  .usage('<commander> [options]')
-  .command('init [filename]', 'create the project').alias('i')
-  .command('update [filename]', 'update the project').alias('u')
-  .command('cache [clean]', 'clean the cache').alias('c')
-  .command('ignore [ls]', 'ignore the file').alias('ig')
-  .command('ignore-add [category]', 'ignore adding the file').alias('ig-a')
-  .command('ignore-rm [category]', 'ignore remove the file').alias('ig-r')
-  .parse(process.argv)
+    .version(require('../package.json').version)
+    .usage('<commander> [options]')
+    .command('init [filename]', 'create the project').alias('i')
+    .command('update [filename]', 'update the project').alias('u')
+    .command('cache [clean]', 'clean the cache').alias('c')
+    .command('ignore [ls]', 'ignore the files').alias('ig')
+    .command('ignore-add [category]', 'ignore adding the files').alias('ig-a')
+    .command('ignore-rm [category]', 'ignore rm the files').alias('ig-r')
+    .parse(process.argv)
